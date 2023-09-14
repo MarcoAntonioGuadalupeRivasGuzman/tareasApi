ade97f93a34ae1ed2ab046bc683542a0d14adae3
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-13 21:11:53 -0600

    add code for resovel cors problem

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const app=express();
 
 app.use(express.json());
 
+app.use((req,res, next)=>{
+    res.append('Access-Control-Allow-Origin',['*']);
+    res.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers','Content-type');
+    next();
+});
+
 app.use(router);
 app.use(tareasRoutes);
 
